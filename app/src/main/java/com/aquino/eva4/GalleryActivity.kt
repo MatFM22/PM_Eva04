a782175d8da523b16a21bf20306f0e9a8ee0287d
MatFM22 <mati22ariesforever@gmail.com>
2024-12-06 20:23:21 -0500

    Aquino Basilio, Fernando Matias

---
+++ app/src/main/java/com/aquino/eva4/GalleryActivity.kt
@@ -0,0 +1,23 @@
+package com.aquino.eva4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aquino.eva4.databinding.ActivityGalleryBinding
+import java.io.File
+
+class GalleryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGalleryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityGalleryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val directory = File(getExternalMediaDirs()[0].absolutePath)
+        val files = directory.listFiles() as Array<File>
+
+        val adapter = GalleryAdapter(files.reversedArray())
+        binding.viewPager.adapter = adapter
+    }
+}
